41757815663904708c8410c401d5d3b94a866610
T-Gamer <1985michel>
2024-08-30 17:28:18 -0300

    add pimenta bueno ro

--- newvariaveisglobais.js
+++ newvariaveisglobais.js
@@ -170,7 +170,7 @@ const cidadesRoraima = ["boa vista", "bonfim", "caracarai", "caroebe", "iracema"
     "sao joao da lingua", "sao luiz", "sao severo", "santa rita", "santa teresa"];
 
 const cidadesRondonia = ["ariquemes", "buritis", "cacoal", "candeias do jamari", "canoas", "cidade negra", "cujubim", "diamantina", "espigao do oeste", "espigao d'oeste", "extrema", "jaru", "jirau",
-    "machadinho doeste", "nacional", "nova brasilandia", "nova mamore", "nova monte verde", "porto velho", "presidente medici", "rio preto doeste",
+    "machadinho doeste", "nacional", "nova brasilandia", "nova mamore", "nova monte verde", "pimenta bueno", "porto velho", "presidente medici", "rio preto doeste",
     "rolim de moura", "santa luzia", "santo antonio", "sao francisco do guapore", "vila bela da santissima trindade", "vilhena"];
 
 const cidadesMatoGrossoDoSul = ["agua clara", "alcinopolis", "amambai", "anastacio", "anhembi", "antonio joao", "aparecida do taboado", "bandeirantes", "bataguassu", "bataypora", "bela vista", "bodoquena", "bonito", "campo grande", "caracol", "cassilandia", "chapadao do sul", "corumba",
